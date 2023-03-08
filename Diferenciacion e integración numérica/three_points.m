8956e0ff5b7dadf307ec809061733f017064368c
Camilohoyos99 <33555587+Camilohoyos99@users.noreply.github.com>
2023-03-07 22:00:44 -0500

    diferenciacioón

---
+++ Diferenciacion e integración numérica/three_points.m
@@ -0,0 +1,11 @@
+function [derivada,error] = three_points(x0,h,f,f3x)
+a=x0-h;
+b=x0+h;
+epsilon = rand(1) *(b-a)+a;
+error= (h^2/6)* f3x(epsilon);
+
+derivada = 1/(2*h)*(f(x0+h)-f(x0-h))-error;
+
+
+end
+
